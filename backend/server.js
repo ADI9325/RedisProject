7abff1474acdf9adaaf2dd3bb7dcf8fab5eb9ced
ADITYA BAGADE <adityabagade04@gmail.com>
2024-08-27 09:06:24 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ if (cluster.isMaster) {
         cluster.fork();
     });
 } else {
-    const PORT = process.env.PORT || 3001;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
